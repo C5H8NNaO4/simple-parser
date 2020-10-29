80d9e3e474a58bdf547773dfbc4fa3c336498fbd
Moritz Roessler <moritz.roessler@gmail.com>
2020-10-29 04:06:14 +0100

    removed console.logs

--- interpreter/Behaviour.js
+++ interpreter/Behaviour.js
@@ -1,13 +1,11 @@
 class Behaviour {
     static eval (ast, behaviours) {
         const node = ast;
-        console.log ("Node", node)
         const beh  = behaviours.find(b => b.testFn(ast)); 
         if (!beh)
             throw new Error(`No behaviour found for node ${JSON.stringify(node)}`)
         return beh.evalFn(node, (node, _behaviours = behaviours) => {
             const val = Behaviour.eval(node, _behaviours)
-            console.log (node.id, val);
             return val;
         });
     }
